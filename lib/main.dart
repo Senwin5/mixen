2ec0ada3e5b0d653df7302d84f9bd9969a6079ae
Senwin.ng <godwinsenwin@outlook.com>
2026-02-18 11:39:37 +0100

    Update of the old code

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,  
       title: 'Mixen',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
